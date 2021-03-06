f5446363c29e8352d6bce60fcb7b746ee425db61
Erlangga <erlangga@cactiva.app>
2021-03-06 20:16:43 +0700

    fix

--- web/src/Routemap.tsx
+++ web/src/Routemap.tsx
@@ -2,15 +2,15 @@ import { Router } from "@reach/router";
 import { observer } from "mobx-react-lite";
 import Theme from "Page/Theme";
 import Landing from "Page/Landing";
-import Main from "Page/Theme/Main/Page";
+// import Main from "Page/Theme/Main/Page";
 
 export default observer(() => {
   return (
     <Router>
       <Landing path="/" />
       <Theme path="/Theme">
-        <Main path="/Theme/Main" />
-        <Main path="/Theme/Main/:sess/:guest" />
+        {/* <Main path="/Theme/Main" />
+        <Main path="/Theme/Main/:sess/:guest" /> */}
       </Theme>
     </Router>
   );
